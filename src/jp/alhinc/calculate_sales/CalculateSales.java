a82859121a6b1511e4a40d52f17fb0add23d7d8c
fukushima_kanako <fukushima.kanako@alhinc.jp>
2024-10-10 15:28:59 +0900

    first commit

---
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -0,0 +1,107 @@
+package jp.alhinc.calculate_sales;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculateSales {
+
+	// 支店定義ファイル名
+	private static final String FILE_NAME_BRANCH_LST = "branch.lst";
+
+	// 支店別集計ファイル名
+	private static final String FILE_NAME_BRANCH_OUT = "branch.out";
+
+	// エラーメッセージ
+	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
+	private static final String FILE_NOT_EXIST = "支店定義ファイルが存在しません";
+	private static final String FILE_INVALID_FORMAT = "支店定義ファイルのフォーマットが不正です";
+
+	/**
+	 * メインメソッド
+	 *
+	 * @param コマンドライン引数
+	 */
+	public static void main(String[] args) {
+		// 支店コードと支店名を保持するMap
+		Map<String, String> branchNames = new HashMap<>();
+		// 支店コードと売上金額を保持するMap
+		Map<String, Long> branchSales = new HashMap<>();
+
+		// 支店定義ファイル読み込み処理
+		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
+			return;
+		}
+
+		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+
+
+
+		// 支店別集計ファイル書き込み処理
+		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
+			return;
+		}
+
+	}
+
+	/**
+	 * 支店定義ファイル読み込み処理
+	 *
+	 * @param フォルダパス
+	 * @param ファイル名
+	 * @param 支店コードと支店名を保持するMap
+	 * @param 支店コードと売上金額を保持するMap
+	 * @return 読み込み可否
+	 */
+	private static boolean readFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
+		BufferedReader br = null;
+
+		try {
+			File file = new File(path, fileName);
+			FileReader fr = new FileReader(file);
+			br = new BufferedReader(fr);
+
+			String line;
+			// 一行ずつ読み込む
+			while((line = br.readLine()) != null) {
+				// ※ここの読み込み処理を変更してください。(処理内容1-2)
+				System.out.println(line);
+			}
+
+		} catch(IOException e) {
+			System.out.println(UNKNOWN_ERROR);
+			return false;
+		} finally {
+			// ファイルを開いている場合
+			if(br != null) {
+				try {
+					// ファイルを閉じる
+					br.close();
+				} catch(IOException e) {
+					System.out.println(UNKNOWN_ERROR);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 支店別集計ファイル書き込み処理
+	 *
+	 * @param フォルダパス
+	 * @param ファイル名
+	 * @param 支店コードと支店名を保持するMap
+	 * @param 支店コードと売上金額を保持するMap
+	 * @return 書き込み可否
+	 */
+	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
+		// ※ここに書き込み処理を作成してください。(処理内容3-1)
+
+		return true;
+	}
+
+}
